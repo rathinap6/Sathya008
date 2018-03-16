6d93773b12a5a51e89e72fc579b88dac680fac49
rathinap6 <35958265+rathinap6@users.noreply.github.com>
2018-03-16 04:21:55 -0700

    Create concatenatestrings.c

---
+++ concatenatestrings.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[100],b[100];
+int n,i,l1,l2;
+printf("Enter the String 1:");
+gets(a);
+printf("Enter the String 2:");
+gets(b);
+printf("%s%s",a,b);
+}
